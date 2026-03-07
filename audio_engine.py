27f194194b8d33d01d723e624804c9c404f9be3c
aakash <dr282924@gmail.com>
2026-03-07 10:26:52 +0530

    Update audio_engine.py

--- audio_engine.py
+++ audio_engine.py
@@ -6,7 +6,7 @@ import uuid
 
 # Cloudinary configuration (Render ke Environment Variables se uthayega)
 cloudinary.config( 
-  cloud_name = os.environ.get("CLOUDINARY_NAME"), 
+  cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME"), 
   api_key = os.environ.get("CLOUDINARY_API_KEY"), 
   api_secret = os.environ.get("CLOUDINARY_API_SECRET") 
 )
